84ccd88e20aa5663f1b9b0b8d0e53ab2b12ec1af
amaurydetremerie <amaury.detremerie@gmail.com>
2024-08-31 02:10:24 +0200

    add reset and daemon reload

--- Webhook/scripts/pull.sh
+++ Webhook/scripts/pull.sh
@@ -3,10 +3,11 @@
 case $1 in
 
   traefik)
-    echo "git -C /etc/Homelab pull origin main" | ssh root@10.100.0.1 /bin/bash
+    echo "git -C /etc/Homelab reset --hard;git -C /etc/Homelab pull origin main" | ssh root@10.100.0.1 /bin/bash
     exit 0
         ;;
   webhook)
+    git -C /etc/Homelab reset --hard
     git -C /etc/Homelab pull origin main
     exit 0
         ;;
--- Webhook/scripts/restart.sh
+++ Webhook/scripts/restart.sh
@@ -3,10 +3,14 @@
 case $1 in
 
   traefik)
-    echo "systemctl restart traefik.service" | ssh root@10.100.0.1 /bin/bash
+    echo "systemctl daemon-reload; systemctl restart traefik.service" | ssh root@10.100.0.1 /bin/bash
     exit 0
     ;;
-
+  webhook)
+    systemctl daemon-reload
+    systemctl restart webhook.service
+    exit 0
+    ;;
   *)
     exit 1
     ;;
